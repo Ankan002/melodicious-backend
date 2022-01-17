41b78ea4d699136c3beba2fecd9b33d8dca47967
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2022-01-17 11:38:52 +0530

    feat: :sparkles: Routes for Updating  Username

--- controllers/user.js
+++ controllers/user.js
@@ -28,11 +28,54 @@ exports.getUserById = async(req, res) => {
     const userId = req.params.userId
     
     try{
-        const user = await User.findById(userId).populate('followers').populate('following').exec()
+        const user = await User.findById(userId).populate('followers').populate('following').select('-liked_songs').exec()
 
         res.status(200).json({
             success: true,
             user 
+        })
+    }
+    catch(error){
+        console.error(error.message)
+        res.status(500).json({
+            success: false,
+            message: 'Internal Server Error!!'
+        })
+    }
+}
+
+//A route to update a user's username. It requires that user to be logged in.
+exports.updateUsername = async(req, res) => {
+    const {username} = req.body
+
+    if(username.length >= 4  && (username.substring(username.length - 4)) === '_gal'){
+        return res.status(200).json({
+            success: false,
+            message: 'Username should not conatain "_gal" at the end'
+        })
+    }
+
+    try{
+        const isThereSameUsername = await User.countDocuments({username})
+
+        if(isThereSameUsername > 0){
+            return res.status(200).json({
+                success: false,
+                message: 'User with the same username already exists'
+            })
+        }
+
+        const userId = req.user
+
+        const newUser = {}
+
+        if(username) newUser.username = username
+
+        const user =  await User.findByIdAndUpdate(userId, {$set: newUser}, {new: true})
+
+        res.status(200).json({
+            success: true,
+            user
         })
     }
     catch(error){
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const {fetchUser} = require('../middlewares/fetchUser') 
-const {getUser, getUserById} = require('../controllers/user')
+const {getUser, getUserById, updateUsername} = require('../controllers/user')
+const {body} = require('express-validator')
 
 //A route to get the user by the jwt access token. Certain super powers are given to this user. You need to be logged in to do so
 router.get('/user', fetchUser, getUser)
 
 //A route to get a single user by their id. No super powers are given in this route more likely for stalking other users. You need to be logged in to do so.
 router.get('/user/:userId', fetchUser, getUserById)
+
+//A route to update a user's username. It requires that user to be logged in.
+router.put('/user/username', fetchUser, [
+    body('username').isLength({min: 1, max: 40}).withMessage('The username must be at least 1 character and at most 40 characters long')
+], updateUsername)
 
 
 module.exports = router
